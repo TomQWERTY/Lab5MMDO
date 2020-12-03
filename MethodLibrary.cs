09608695df14494ee1ac6c7a3aef3c0d5f3e37fa
Oleksii Veselskyi <nightinzond@gmail.com>
2020-12-03 09:32:23 +0200

    Some letters added

---
+++ MethodLibrary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5MMDO
+{
+    class MethodLibrary
+    {
+        public static double[] ReiskiMehod(int n, Function F, double[] x0, double h0, double e)
+        {
+
+        }
+
+        private static double GradF(double[] x0)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5MMDO
+{
+    delegate double Function(double x);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
